4c4fe266749c6d9f04477ddde100f9755e391044
Jason Hannanto <hannanto@usc.edu>
2018-06-07 20:17:53 -0700

    Fix PORT error

--- index.js
+++ index.js
@@ -36,5 +36,3 @@ require('./routes/authRoutes')(app);
 app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-console.log("Server running on PORT " + PORT);
